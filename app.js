3cc6b86c88e8047bac98267e9a5cb054853d82ec
Stanislav Minko <minkostas@gmail.com>
2024-01-14 11:25:19 +0300

    :cookie-parser

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 const app = express();
 const user = require('./src/controller/user.controller');
 
+app.use(cookieParser());
 app.use(bodyParser.json());
-app.use((error, req, res, _next)=> res.send(error.message));
 app.use('/user', user);
+
+app.use((error, req, res, _next)=> res.send(error.message));
 
 module.exports = app;
